c4153a91a75579b48439be27205ea1b930cad420
PKUYUEWANG <www8478652110@163.com>
2017-11-20 19:51:28 +0800

    布局

---
+++ app/src/main/java/com/example/gavin/xuansushe/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gavin.xuansushe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by GAVIN on 2017/11/20.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xuansushe);
+    }
+}
